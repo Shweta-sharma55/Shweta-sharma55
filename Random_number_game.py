212d1b6854111efc48387137b2cb2a98902b44b4
Gopi Krishna G <gopikrishnaguntupalli9677@gmail.com>
2022-08-31 14:13:44 +0530

    Two Player logic implemented Removed computer play mode

--- Random_number_game.py
+++ Random_number_game.py
@@ -1,41 +1,44 @@
 # number game
 import random
+import maskpass #pip install maskpass
 
-
-def get_check_input():
-    choice = int(input(f"Please Choose number :"))
+def get_check_input(name):
+    choice = int(maskpass.askpass(prompt=f"{name } Choose a number in range 0-9 : ",mask= "*"))
     # Add input Filtering Logic here
     return choice
 
 
 def game():
-    player_name = input("Please Enter your name : ")
-    player_list = [player_name, "Computer"]
-    player_choice = " "
-    Computer_choice = " "
+    player_1_name = input("player_1 Please Enter your name : ")
+    player_2_name = input("player_2 Please Enter your name : ")
+
+    player_list = [player_2_name,player_1_name]
+    player_1_choice = " "
+    player_2_choice = " "
 
     player_turn = random.choice(player_list)
     print(f"{player_turn} will go first  ")
-    if player_turn == player_name:
-        player_choice = get_check_input()
-        print(player_choice)
+    if player_turn == player_1_name:
+        player_1_choice = get_check_input(player_1_name)
+        print(player_1_choice)
     else:
-        Computer_choice = random.randint(0, 9)
+        player_2_choice = get_check_input(player_2_name)
+        print(player_2_choice)
 
-    if player_choice == " ":
-        player_choice = get_check_input()
-    if Computer_choice == " ":
-        Computer_choice = random.randint(0, 9)
+    if player_1_choice == " ":
+        player_1_choice = get_check_input(player_1_name)
+    if player_2_choice == " ":
+        player_2_choice = get_check_input(player_2_name)
 
-    if (player_choice > Computer_choice):
+    if (player_1_choice > player_2_choice):
         print(
-            f"{player_name} choose {player_choice} and computer choose {Computer_choice} \n >>>>{player_name} win<<<<<<")
-    elif (Computer_choice == player_choice):
+            f"{player_1_name} choose {player_1_choice} and {player_2_name} choose {player_2_choice} \n >>>>{player_1_name} win<<<<<<")
+    elif (player_2_choice == player_1_choice):
         print(
-            f"{player_name} choose {player_choice} and computer choose {Computer_choice} \n >>>>Tie<<<<<<")
+            f"{player_1_name} choose {player_1_choice} and {player_2_name} choose {player_2_choice} \n >>>> Game TIe<<<<<<")
     else:
         print(
-            f"{player_name} choose {player_choice} and computer choose {Computer_choice} \n >>>>Computer win<<<<<<")
+            f"{player_1_name} choose {player_1_choice} and {player_2_name} choose {player_2_choice} \n >>>>{player_2_name} win<<<<<<")
 
     print("play Again ???")
     play_again = input("Play again Y/N :").lower()
